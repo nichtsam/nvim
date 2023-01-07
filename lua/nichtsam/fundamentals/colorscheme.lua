ba5ad3a7f2e701555a9255da6bea7f31429d2405
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-01-07 22:11:17 +0800

    fix(nvim): support multiple colorschemes

--- lua/nichtsam/fundamentals/colorscheme.lua
+++ lua/nichtsam/fundamentals/colorscheme.lua
@@ -4,19 +4,18 @@ local colorscheme = "tokyonight"
 -- setup tokyonight
 local tokyonight_ok, tokyonight = pcall(require, "tokyonight")
 
-if not tokyonight_ok then
+if tokyonight_ok then
+	tokyonight.setup({
+		transparent = true,
+	})
+else
 	vim.notify("tokyonight not found!")
-	return
 end
-
-tokyonight.setup({
-	transparent = true,
-})
 
 -- setup colorscheme
 local colorscheme_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not colorscheme_ok then
-	vim.notify("colorscheme " .. tokyonight .. " not found!")
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
 end
 
