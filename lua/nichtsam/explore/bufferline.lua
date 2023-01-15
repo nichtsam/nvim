d01a32550db846b7423ab52e7f1ec49a8afb8fe5
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-01-15 18:20:19 +0800

    chore: thicken bufferline current buffer indicator

--- lua/nichtsam/explore/bufferline.lua
+++ lua/nichtsam/explore/bufferline.lua
@@ -1,41 +1,41 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-	return
+    return
 end
 
 bufferline.setup({
-	options = {
-		diagnostics = "nvim_lsp",
-		indicator = {
-			style = "icon",
-			icon = "▎",
-		},
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "File Explorer",
-				text_align = "left",
-				highlight = "WildMenu",
-				padding = 1,
-			},
-		},
-		buffer_close_icon = "",
-		close_icon = "",
-	},
-	highlights = {
-		fill = {
-			fg = { attribute = "fg", highlight = "ff0000" },
-			bg = { attribute = "bg", highlight = "@none" },
-		},
+    options = {
+        diagnostics = "nvim_lsp",
+        indicator = {
+            style = "icon",
+            icon = "▌",
+        },
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                highlight = "WildMenu",
+                padding = 1,
+            },
+        },
+        buffer_close_icon = "",
+        close_icon = "",
+    },
+    highlights = {
+        fill = {
+            fg = { attribute = "fg", highlight = "ff0000" },
+            bg = { attribute = "bg", highlight = "@none" },
+        },
 
-		background = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "@none" },
-		},
+        background = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "@none" },
+        },
 
-		separator = {
-			fg = { attribute = "bg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "@none" },
-		},
-	},
+        separator = {
+            fg = { attribute = "bg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "@none" },
+        },
+    },
 })
