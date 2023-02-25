22ff959bddc8314353c23a54c0a69f2daccd24f7
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-02-25 23:48:54 +0800

    fix lua lsp

--- lua/nichtsam/code_intelligence/lsp.lua
+++ lua/nichtsam/code_intelligence/lsp.lua
@@ -5,49 +5,49 @@ lsp.preset("recommended")
 
 -- Install these servers
 lsp.ensure_installed({
-	"tsserver",
-	"eslint",
-	"sumneko_lua",
-	"rust_analyzer",
+    "tsserver",
+    "eslint",
+    "lua_ls",
+    "rust_analyzer",
 })
 
 lsp.set_preferences({
-	set_lsp_keymaps = { omit = { "<F2>", "<F4>" } },
-	sign_icons = {
-		error = "E",
-		warn = "W",
-		hint = "H",
-		info = "I",
-	},
+    set_lsp_keymaps = { omit = { "<F2>", "<F4>" } },
+    sign_icons = {
+        error = "E",
+        warn = "W",
+        hint = "H",
+        info = "I",
+    },
 })
 
 local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 lsp.on_attach(function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = group,
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({ bufnr = bufnr })
-			end,
-		})
-	end
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = group,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+    end
 
-	local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 end)
 
 -- Fix Undefined global 'vim'
-lsp.configure("sumneko_lua", {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+lsp.configure("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
 })
 
 lsp.setup()
@@ -57,14 +57,14 @@ local null_opts = lsp.build_options("null-ls", {})
 local mason_null_ls = require("mason-null-ls")
 
 null_ls.setup({
-	on_attach = function(client, bufnr)
-		null_opts.on_attach(client, bufnr)
-	end,
+    on_attach = function(client, bufnr)
+        null_opts.on_attach(client, bufnr)
+    end,
 })
 
 mason_null_ls.setup({
-	ensure_installed = { "prettier" },
-	automatic_installation = true,
-	automatic_setup = true,
+    ensure_installed = { "prettier" },
+    automatic_installation = true,
+    automatic_setup = true,
 })
 mason_null_ls.setup_handlers({})
